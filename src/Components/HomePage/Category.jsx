6a24e733faf7892214c337f4bc8ebef9554b871f
B <146595822+Za3mon@users.noreply.github.com>
2024-08-23 02:25:23 +0530

    Responsive issues

--- src/Components/HomePage/Category.jsx
+++ src/Components/HomePage/Category.jsx
@@ -6,11 +6,9 @@ const Category = ({item, btn}) => {
 
   const {handleLinkClick}=useContext(products)
   return (
-    <div className=" lg:w-full lg:h-full sm:mx-5 relative sm:w-[450px] sm:my-5 lg:my-0 w-[250px] mt-4">
+    <div className=" lg:w-full lg:h-full sm:mx-5 relative sm:w-[450px] sm:my-5 lg:my-0 h-[90%] mt-4">
       <Link to={item.link} onClick={handleLinkClick}>
-      <div className="">
-      <img src={item.img} alt="" className="lg:w-full lg:h-2/4 h-1/4 rounded-md"/>
-      </div>
+      <img src={item.img} alt="" className="lg:w-full lg:h-2/4 rounded-md"/>  
       <div className="absolute ml-6 2xl:top-[73%] xl:top-[60%] lg:top-[46%] sm:top-[60%] top-[50%]">
       <h1 className="sm:text-xl text-lg  text-white">{item.title}</h1>
       <h1 className="sm:text-3xl text-xl text-white">{item.tagline}</h1>
