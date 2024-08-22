1e3fce801ebfb71cb658de62f4647a199e4c41da
B <146595822+Za3mon@users.noreply.github.com>
2024-08-23 02:11:03 +0530

    fixed mobile view

--- src/Components/HomePage/Category.jsx
+++ src/Components/HomePage/Category.jsx
@@ -6,14 +6,14 @@ const Category = ({item, btn}) => {
 
   const {handleLinkClick}=useContext(products)
   return (
-    <div className=" lg:w-full lg:h-full sm:mx-5 relative sm:w-[450px] sm:my-5 lg:my-0 w-[330px] mt-4">
+    <div className=" lg:w-full lg:h-full sm:mx-5 relative sm:w-[450px] sm:my-5 lg:my-0 w-[250px] mt-4">
       <Link to={item.link} onClick={handleLinkClick}>
       <div className="">
-      <img src={item.img} alt="" className="lg:w-full h-2/4 rounded-md"/>
+      <img src={item.img} alt="" className="lg:w-full lg:h-2/4 h-1/4 rounded-md"/>
       </div>
       <div className="absolute ml-6 2xl:top-[73%] xl:top-[60%] lg:top-[46%] sm:top-[60%] top-[50%]">
-      <h1 className="text-xl text-white">{item.title}</h1>
-      <h1 className="text-3xl text-white">{item.tagline}</h1>
+      <h1 className="sm:text-xl text-lg  text-white">{item.title}</h1>
+      <h1 className="sm:text-3xl text-xl text-white">{item.tagline}</h1>
       <HomepageBtn btnValue={btn}></HomepageBtn>
       </div>
       </Link>
--- src/Components/HomePage/HomepageBtn.jsx
+++ src/Components/HomePage/HomepageBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomepageBtn = ({btnValue, isHeadingBtn}) => {
   return (
-    <button className={`mt-3  py-2 px-6 rounded-3xl mr-3 font-semibold ${isHeadingBtn ? 'bg-black text-white': 'bg-white' }`}>{btnValue}</button>
+    <button className={`mt-3 py-2 sm:px-6 px-4 rounded-3xl mr-3 sm:text-base text-sm font-semibold ${isHeadingBtn ? 'bg-black text-white': 'bg-white' }`}>{btnValue}</button>
   )
 }
 
